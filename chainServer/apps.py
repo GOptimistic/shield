7c4da1d6a7e37412fee0471a6f3dcfc39c3627cd
marui <marui99@foxmail.com>
2019-08-27 11:32:53 +0800

    add new apps

---
+++ chainServer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChainserverConfig(AppConfig):
+    name = 'chainServer'
--- shield/settings.py
+++ shield/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import pymysql
+
 pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shieldServer',
+    'chainServer',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shield.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,12 +79,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shield',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'LgHmL_SHIELD!0827.',
         'HOST': '49.232.23.19',
+        'PORT': '3306',
+    },
+    'chain_db': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blockchain',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -105,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -119,9 +124,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR),'shieldServer/../static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR), 'shieldServer/../static')
+
+# use multi-database in django
+DATABASE_ROUTERS = ['shield.database_router.DatabaseAppsRouter']
+DATABASE_APPS_MAPPING = {
+    'shieldServer': 'default',
+    'chainServer': 'chain_db',
+}
