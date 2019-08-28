d7282e7fed002a63e60f80c0e61ec83f92fa132a
marui <marui99@foxmail.com>
2019-08-28 19:33:56 +0800

    回车登录

--- static/js/login.js
+++ static/js/login.js
@@ -1,19 +1,25 @@
 //for login in the index page
-function ajaxResponse(xhr,successFunction,falseFunction) {
-     xhr.onreadystatechange = function () {
-         if (xhr.readyState === 4) {
-             console.log(xhr.status);
-             if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-                 //alert("成功");
-                 successFunction();
-             } else {
-                 // alert("失败" + xhr.status);
-                 document.getElementById("login_name_j").innerHTML = "用户名或密码为空";
-                 falseFunction();
-             }
-         }
-     }
- };
+function ajaxResponse(xhr, successFunction, falseFunction) {
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+                //alert("成功");
+                successFunction();
+            } else {
+                // alert("失败" + xhr.status);
+                document.getElementById("login_name_j").innerHTML = "用户名或密码为空";
+                falseFunction();
+            }
+        }
+    }
+};
+
+//回车时，默认是登陆
+function keyLogin() {
+    if (event.keyCode === 13)  //回车键的键值为13
+        document.getElementById("login-button").click(); //调用登录按钮的登录事件
+}
 
 let subBt = document.getElementById('login-button');
 subBt.onclick = function () {
@@ -26,8 +32,8 @@ subBt.onclick = function () {
             let respones = JSON.parse(xhrRegister.responseText);
             document.getElementById("login_name_j").innerHTML = respones.msg;
             location.assign('http://127.0.0.1:8000/index/home_after.html');
-        },function () {
-            });
+        }, function () {
+        });
     let user = {
         userID: userID,
         pwd: pwd
